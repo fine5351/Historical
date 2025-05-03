02f95751f6b864d80c4b1be32b0f9cc648fa8198
eric.kuo <fine1101105351@gmail.com>
2025-05-03 17:30:42 +0800

    feat: add module

---
+++ normal-core/src/main/java/com/finekuo/normalcore/NormalCoreApplication.java
@@ -0,0 +1,13 @@
+package com.finekuo.normalcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormalCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NormalCoreApplication.class, args);
+    }
+
+}
