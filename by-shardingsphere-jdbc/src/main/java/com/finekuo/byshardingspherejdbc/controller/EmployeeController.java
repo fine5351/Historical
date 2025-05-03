46b9940b426c5f320279a14220989a43bf3d6a71
eric.kuo <fine1101105351@gmail.com>
2025-05-03 23:59:07 +0800

    feat: refactor

--- by-shardingsphere-jdbc/src/main/java/com/finekuo/byshardingspherejdbc/controller/EmployeeController.java
+++ by-shardingsphere-jdbc/src/main/java/com/finekuo/byshardingspherejdbc/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.finekuo.byshardingspherejdbc.controller;
 
 import com.finekuo.mybatisflexcore.entity.Employee;
 import com.finekuo.mybatisflexcore.mapper.EmployeeMapper;
+import com.finekuo.normalcore.dto.response.BaseResponse;
 import com.mybatisflex.core.paginate.Page;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -22,43 +23,44 @@ public class EmployeeController {
 
     @GetMapping
     @Operation(summary = "Get all employees", description = "Retrieves a list of all employees")
-    public List<Employee> getAllEmployees() {
-        return employeeMapper.selectAll();
+    public BaseResponse<List<Employee>> getAllEmployees() {
+        return BaseResponse.success(employeeMapper.selectAll());
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "Get employee by ID", description = "Retrieves an employee by their ID")
-    public Employee getEmployeeById(@PathVariable Long id) {
-        return employeeMapper.selectOneById(id);
+    public BaseResponse<Employee> getEmployeeById(@PathVariable Long id) {
+        return BaseResponse.success(employeeMapper.selectOneById(id));
     }
 
     @PostMapping
     @Operation(summary = "Create employee", description = "Creates a new employee")
-    public Employee createEmployee(@RequestBody Employee employee) {
+    public BaseResponse<Employee> createEmployee(@RequestBody Employee employee) {
         employeeMapper.insert(employee);
-        return employee;
+        return BaseResponse.success(employee);
     }
 
     @PutMapping("/{id}")
     @Operation(summary = "Update employee", description = "Updates an existing employee")
-    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+    public BaseResponse<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
         employee.setId(id);
         employeeMapper.update(employee);
-        return employee;
+        return BaseResponse.success(employee);
     }
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete employee", description = "Deletes an employee by their ID")
-    public void deleteEmployee(@PathVariable Long id) {
+    public BaseResponse<Void> deleteEmployee(@PathVariable Long id) {
         employeeMapper.deleteById(id);
+        return BaseResponse.success();
     }
 
     @GetMapping("/page")
     @Operation(summary = "Get employees with pagination", description = "Retrieves a paginated list of employees")
-    public Page<Employee> getEmployeesWithPagination(
+    public BaseResponse<Page<Employee>> getEmployeesWithPagination(
             @RequestParam(defaultValue = "1") int pageNumber,
             @RequestParam(defaultValue = "10") int pageSize) {
-        return employeeMapper.paginate(pageNumber, pageSize, select());
+        return BaseResponse.success(employeeMapper.paginate(pageNumber, pageSize, select()));
     }
 
 }
